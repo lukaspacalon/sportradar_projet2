af390ac70e4af523500c1df20d778504d2651b2e
lkcoredo <94901317+lkcoredo@users.noreply.github.com>
2024-09-07 15:28:16 +0200

    Start again // other solution

--- football-scoreboard (1)/football-scoreboard/src/test/java/com/example/football_scoreboard/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.example.football_scoreboard;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
